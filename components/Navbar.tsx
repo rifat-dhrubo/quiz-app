51d39a553ef472fdcd7e22ad98b4d22a4ba50984
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2022-03-24 15:35:33 +0600

    fix:(head): added missing head tag

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,21 @@
 import Link from 'next/link';
 import React from 'react';
-
+import Head from 'next/head';
 const Navbar = () => {
   return (
-    <nav className="flex justify-start w-full px-12 py-4 bg-baseColor">
-      <Link passHref href="/">
-        <a className="text-lg font-bold sm:text-xl text-secondary">
-          Britannica Quiz
-        </a>
-      </Link>
-    </nav>
+    <>
+      <Head>
+        <title>Britannica Quiz</title>
+        <meta name="description" content="A delightful online quiz platform" />
+      </Head>
+      <nav className="flex justify-start w-full px-12 py-4 bg-baseColor">
+        <Link passHref href="/">
+          <a className="text-lg font-bold sm:text-xl text-secondary">
+            Britannica Quiz
+          </a>
+        </Link>
+      </nav>
+    </>
   );
 };
 
